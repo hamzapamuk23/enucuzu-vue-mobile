5a8cccc97727b0f2b2e67e93ece9d9a98646a7a4
hamzapamuk23 <h.pmk236023@gmail.com>
2022-06-04 11:25:19 +0300

    feat: first commit

---
+++ src/views/ProductDetailMobile.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-row class="mt-5 mx-2 elevation-3" width="100%" align="center">
+        <v-col width="50%" >
+            <v-card max-width="35%" class="elevation-3" style=" display:inline-block; overflow:hidden; white-space: nowrap; text-overflow: ellipsis;" >
+                <v-img :src="product.imageUrl"></v-img>
+                <v-card-title class="pa-0" style=" font-size:10px; font-family:source_sans_proregular;">{{product.name}}</v-card-title>
+                <v-divider class="mx-4"></v-divider>
+                <v-card-subtitle align="center" class="pa-2" style="; font-size:50%"><strong> {{product.price}} TL ({{product.platformName}})</strong></v-card-subtitle>
+            </v-card>
+            <v-div class="ml-5"  style="display:inline-block; position: relative; " >
+                <v-btn  style="display:block">Trendyol:<span>{{product.price}} TL</span></v-btn>
+                <v-btn class="mt-2" style="display:block">Trendyol:<span>{{product.price}} TL</span></v-btn>
+            </v-div>
+        </v-col>
+        <v-col   width="50%">
+            <table>
+                <tr>
+                    <th style="border-bottom:1px solid grey" class="text-left">Teknik Özellikler:</th>
+                </tr>
+                <tr v-for="(productDetail) in Object.entries(this.product.detail)" :key="productDetail.id">
+                    <th class="text-left" style="font-size: 90%; width: 60%">{{productDetail[0]}}</th>
+                    <th class="text-left" style="width: 3%">:</th>
+                    <td style="width: 37%; font-size: 80%;" colspan="2">{{productDetail[1]}}</td>
+                </tr>
+            </table>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        product:{},
+        otherLinks:[{url:"https://cdn.dsmcdn.com/web/logo/ty-web.svg",name:"N11"},
+        {url:"https://www.trendyol.com/samsung-c-t/gt-e-1205-kamerasiz-tuslu-cep-telefonu-p-258163943",name:"N11"},
+        {url:"https://www.trendyol.com/samsung-c-t/gt-e-1205-kamerasiz-tuslu-cep-telefonu-p-258163943",name:"N11"}]
+    }),
+    methods:{
+        async getProduct(){
+            const response = await this.axios.get("http://localhost:8080/product/search/getProduct?id=" + this.$route.query.productId)
+            this.product = response.data._embedded.products[0]
+            console.log(this.product)
+
+        }
+    },
+    mounted(){
+        this.getProduct()
+    }
+}
+</script>
